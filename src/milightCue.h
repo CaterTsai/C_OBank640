fb46abdea05009569f90b7a14e3572428a70ae5a
Cater Tsai <caterokusai@gmail.com>
2019-01-17 23:07:09 +0800

    add GUI

--- src/milightCue.h
+++ src/milightCue.h
@@ -14,11 +14,17 @@ enum eMilightCueType {
 class milightCue {
 public:
 	milightCue()
+		: _type(eCueStay)
+		,_warmWhiteVal(0)
+		,_coldWhiteVal(0)
+		, _cueDuration(0.0f)
+		, _loopDuration(0.0f)
 	{}
 
 public:
 	eMilightCueType _type;
-	ofColor _color;
+	ofColor _color1, _color2;
 	int _warmWhiteVal, _coldWhiteVal;
+	float _cueDuration, _loopDuration;
 
 };
--- src/milightMgr.h
+++ src/milightMgr.h
@@ -1,12 +1,49 @@
 #pragma once
 
+#include "ofxXmlSettings.h"
+#include "ofxGui.h"
 #include "milightCtrl.h"
+#include "milightCue.h"
+
+typedef vector<milightCue> milightCueTable;
 
 class milightMgr
 {
 public:
+	void setup();
+	void update();
+
+	void drawLight();
+	void drawCtrl();
+
+	void load();
+	void save();
+
+	//----------------------
+	//Cue	
+	void toCue(int cueNo);
+	void nextCue();
+	void prevCue();
+
+	void addCue();
+	void delCue();
+
 
 private:
-	list<milightCtrl> _lightList;
+	void initGUI();
 
+private:
+	bool _isSetup;
+	int _cueNo;
+	vector<milightCtrl> _lightList;
+	vector<milightCueTable> _cueTable;
+
+	//GUI
+	ofxGuiGroup _lightGUI;
+	ofxColorSlider _gCLightColor;
+	ofxFloatSlider _gSWarmWhite, _gSColdWhite;
+	ofxFloatSlider _gSCueDuration, _gSLoopDuration;
+	
+	ofxButton _gBAddCue, _gBDelCue, _gBUpdate, _gBHasLoop;
+	
 };
